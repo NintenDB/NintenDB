d0937993781cf3941cf8b4a3334521fabe25d6fe
Aks <akstewart@protonmail.com>
2020-02-22 18:17:46 +0530

    Co-authored-by: avelican <funnyav@gmail.com>

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <head>
     <title>Hi</title>
 </head>
+
 <?php
+
+for($i =0; $i < 10; $i++){
+    echo $i." bottles of beer on the wall<br>";
+}
 
 echo "hello php";
 ?>
@@ -10,3 +15,5 @@ echo "hello php";
 Are you still here?
 
 hello there
+
+test 3911 876 876 876
