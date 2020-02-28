0a5428c2fe7b293def6747a717dc6f017145dc12
avelican <funnyav@gmail.com>
2020-02-28 08:21:07 +0100

    show_games.php displays results in a table

--- -/php/show_games.php
+++ -/php/show_games.php
@@ -1,3 +1,11 @@
+<style>
+	th {
+    text-align: left;
+    padding: 6px 4px 6px 0px;
+    border-bottom: 1px solid grey;
+	}
+</style>
+
 <?php
 
 $charset = "utf8mb4";
@@ -25,7 +33,46 @@ try {
 	throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 
-$stmt = $pdo->query('SELECT * FROM games');
+// $stmt = $pdo->query('SELECT * FROM games');]
+$stmt = $pdo->query('SELECT name, franchise, console, genre, players, developer, publisher, year FROM games');
+
+
+
+
+
+
+
+
+echo '<table cellspacing="0">';
+$firstRow = true;
+
+while ($row = $stmt->fetch()){
+	echo "<tr>";
+	// todo nicer way to do this? 
+	if($firstRow){
+		echo '<tr class="firstRow">';
+		foreach(array_keys($row) as $column){
+			echo "<th>$column</th>";
+		}
+		$firstRow = false;
+	}else{
+		foreach($row as $item){
+			echo "<td>$item</td>";
+		}
+	}
+	echo "</tr>";
+
+}
+
+
+
+echo "</table>";
+
+
+
+
+
+
 
 echo "<pre>\n";
 
@@ -38,4 +85,10 @@ while ($row = $stmt->fetch()){
 }
 
 echo "</pre>\n";
+
+
+
+
+
+
 ?>
