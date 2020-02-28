2311d223db6ba0e0af2f7fc844f170b64c9376ab
Aks <untitledaks@gmail.com>
2020-02-28 21:02:23 +0530

    responsive resizing for 1, 2 and 3 columns

--- index.php
+++ index.php
@@ -4,16 +4,16 @@
         <title>NintenDB</title>
         <style>
         </style>
-        <link href="+/styles-v.css" rel="stylesheet" type="text/css" />
+        <link href="+/styles-v-r.css" rel="stylesheet" type="text/css" />
     </head>
 <body>
 
-<div id="index">
+<div class="index">
     <div id="title">
         <h1>NintenDB</h1>
     </div>
 
-    <div id="nav">
+    <div id="nav" class="aside">
         <div id="nav-site">
             <ul>
                 <li>Browse</li>
@@ -41,14 +41,68 @@
             </ul>
         </div>
     </div>
-
-   
     
     <div id="main">
+
         <h2>Main</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
+
+</p>
+        
+        <!-- <div id="listings">
+            <h3>Listings</h3>
+            <div class="item">
+                <h4>Game</h4>
+                <img src="#" class="thumbnail">
+                <p class="description">Quod aliquam libero libero laudantium vitae voluptatem. Ut voluptates eos eum dolores possimus quia repellendus recusandae.
+                </p>
+                <ul class="info">
+                    <li>Info 1</li>
+                    <li>Info 2</li>
+                    <li>Info 3</li>
+                </ul>
+            </div>
+            
+            <div class="item">
+                <h4>Game</h4>
+                <img src="#" class="thumbnail">
+                <p class="description">Quod aliquam libero libero laudantium vitae voluptatem. Ut voluptates eos eum dolores possimus quia repellendus recusandae.
+                </p>
+                <ul class="info">
+                    <li>Info 1</li>
+                    <li>Info 2</li>
+                    <li>Info 3</li>
+                </ul>
+            </div>
+
+            <div class="item">
+                <h4>Game</h4>
+                <img src="#" class="thumbnail">
+                <p class="description">Quod aliquam libero libero laudantium vitae voluptatem. Ut voluptates eos eum dolores possimus quia repellendus recusandae.
+                </p>
+                <ul class="info">
+                    <li>Info 1</li>
+                    <li>Info 2</li>
+                    <li>Info 3</li>
+                </ul>
+            </div>
+
+            <div class="item">
+                <h4>Game</h4>
+                <img src="#" class="thumbnail">
+                <p class="description">Quod aliquam libero libero laudantium vitae voluptatem. Ut voluptates eos eum dolores possimus quia repellendus recusandae.
+                </p>
+                <ul class="info">
+                    <li>Info 1</li>
+                    <li>Info 2</li>
+                    <li>Info 3</li>
+                </ul>
+            </div>
+        </div> -->
+
     </div>
 
-    <div id="sidebar">
+    <div id="sidebar" class="aside">
         <h2>Sidebar</h2>
     </div>
 
